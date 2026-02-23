622185b303da872774b7299d529050e9738efa63
novanovich <sc6rkmmch5@privaterelay.appleid.com>
2026-02-23 13:17:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("loveBtn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", function () {
+  message.textContent =
+    "I love you more than words, more than code, more than anything ❤️";
+});
